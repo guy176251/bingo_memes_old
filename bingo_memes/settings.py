9bb2907238c1a205c2b4a6439363f7e3712f5aa0
long <longnguyen176251@gmail.com>
2021-07-08 10:03:13 -0500

    modified:   bingo_memes/settings.py 	modified:   scripts/db_name.py 	new file:   scripts/nginx-setup

--- bingo_memes/settings.py
+++ bingo_memes/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 from decouple import config, Csv
 
+APP_NAME = 'bingo_memes'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 REACT_BUILD_DIR = BASE_DIR / 'frontend' / 'build'
--- scripts/db_name.py
+++ scripts/db_name.py
@@ -7,7 +7,14 @@ path = pathlib.Path(__file__).absolute().parent.parent
 os.chdir(path)
 os.sys.path.append(str(path))
 
-from bingo_memes.settings import DATABASES, WSGI_APPLICATION
+from bingo_memes.settings import (
+    DATABASES,
+    WSGI_APPLICATION,
+    STATICFILES_DIRS,
+    STATIC_URL,
+    APP_NAME,
+)
+
 db_name = DATABASES['default']['NAME']
 
 if __name__ == '__main__':
